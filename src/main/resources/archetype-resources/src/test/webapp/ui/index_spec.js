68dc456a50cc54a5d41f2006e4a67d6cf6e3e455
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2011-08-01 17:18:33 +0200

    udpdate jasmine-ui and us testwindow() in archetype sample code

--- src/main/resources/archetype-resources/src/test/webapp/ui/index_spec.js
+++ src/main/resources/archetype-resources/src/test/webapp/ui/index_spec.js
@@ -2,7 +2,7 @@ describe("index", function () {
     it("should show hello world", function () {
         loadHtml("index.html");
         runs(function () {
-            var greeting = testframe().document.getElementById("greeting").innerHTML;
+            var greeting = testwindow().document.getElementById("greeting").innerHTML;
             expect(greeting).toEqual("Hello World!");
         });
     });
